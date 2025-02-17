1f424c1b0b551b4dfacd7791bc28bf53a7862628
Maria_Gudima <maritagudima@gmail.com>
2025-02-17 16:59:22 +0100

    upload sql file

---
+++ ProjectCopy/1_top_paying_jobs_copy.sql
@@ -0,0 +1,17 @@
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name as company_name 
+from 
+    job_postings_fact 
+left join company_dim on job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    job_title_short = 'Data Analyst' AND 
+    job_location = 'Anywhere' AND
+    salary_year_avg is not NULL
+order by salary_year_avg DESC
+limit 10
